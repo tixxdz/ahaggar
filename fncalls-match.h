ee899ea0f33688eab6b1597830bd8147dd67585f
Djalal Harouni <tixxdz@opendz.org>
2013-04-09 23:32:56 +0100

    Ahaggar: add the fncalls-match.h header file
    This file contains declaration of structures and functions
    that are used to store, search and lookup functions in an effective way
    using hash tables and static arrays.
    
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

---
+++ fncalls-match.h
@@ -0,0 +1,58 @@
+/*
+ * The Ahaggar framework
+ *
+ * Copyright (C) 2012-2013  Djalal Harouni <tixxdz@opendz.org>
+ * Copyright (C) 2012-2013  LIRE Laboratory.
+ * University Constantine 2, Algeria.
+ *
+ *   Author(s): Djalal Harouni <tixxdz@opendz.org>
+ *              Pr. Mohamed Benmohammed
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _FNCALLS_MATCH_H
+#define _FNCALLS_MATCH_H
+
+enum __hash_ids {
+	errors_hash,
+	warns_hash,
+	reports_hash,
+};
+
+struct pattern_match {
+	char *args;
+	char *all;
+	char *msg;
+};
+
+struct target_functions {
+	const char *name;
+	struct pattern_match *patterns;
+};
+
+struct hash_functions {
+	htab_t tab;
+	struct target_functions *targets;
+
+	/* number of elements of the target_functions array.
+	 * Currently it must be know at compile time */
+	unsigned int targets_size;
+};
+
+int fncalls_match_init(void);
+void fncalls_match_finish(void);
+
+int match_function_name(void *data, void *plug_data);
+int match_handle_output(void *plug_data);
+
+#endif /* _FNCALLS_MATCH_H */
