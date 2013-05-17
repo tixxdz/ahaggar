5906db89e351e1a2ca935bcea8cb9ffe57160585
Djalal Harouni <tixxdz@opendz.org>
2013-05-17 18:23:00 +0100

    Ahaggar: add a file that contains some constants checks
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

---
+++ fncalls_checks/const-reports.h
@@ -0,0 +1,42 @@
+/* Misc check for constants defined during compile time */
+{
+	.name = "open",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.args = "^\\((.*(<string_cst>)+.*),)?.*\\)$",
+		.all = NULL,
+		.msg_nomatch = "function 'open' pathname argument is not a compile time const"
+		},
+		{
+		.active = 1,
+		.args = "^\\((.*,)?(.*(<integer_cst>)+.*)+\\)$",
+		.msg_nomatch = "function 'open' flags or mode arguments are not compile time const"
+		},
+		{ NULL },
+	},
+},
+{
+	.name = "malloc",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.match_args_func = malloc_chk_allocation_size,
+		},
+		{ NULL },
+	}
+},
+{
+	.name = "free",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.args = "^\\(<pointer_type>\\s<integer_cst>\\s0\\)$",
+		.msg = "function 'free' is nop, ptr argument is NULL"
+		},
+		{ NULL },
+	},
+},
+{
+	.name = "memset",
+},
