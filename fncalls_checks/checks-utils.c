dfb4aaef970181103e09d39c00745582568bc64a
Djalal Harouni <tixxdz@opendz.org>
2013-05-20 14:34:41 +0100

    Ahaggar: define two new helper functions
    * get_next_arg() to retrieve the next argument in the function call
    * get_args_nr() to get the number of arguments.
    
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

--- fncalls_checks/checks-utils.c
+++ fncalls_checks/checks-utils.c
@@ -28,16 +28,60 @@
 
 #include "checks-utils.h"
 
+char *get_next_arg(char **call_offset)
+{
+	unsigned int subcalls = 0;
+	char *end = NULL;
+	char *ptr = *call_offset;
+
+	if (!ptr)
+		return ptr;
+
+	if (*ptr == '(')
+		ptr++;
+
+	if (*ptr == ')' || *ptr == '\0') {
+		ptr = NULL;
+		goto out;
+	}
+
+	for (end = ptr; *end != '\0'; end++) {
+		if (*end == '(') {
+			subcalls++;
+		} else if (*end == ')') {
+			if (subcalls)
+				subcalls--;
+			else
+				break;
+		} else if (*end == ',' && subcalls == 0) {
+			end += 2;
+			break;
+		}
+	}
+
+out:
+	*call_offset = end;
+	return ptr;
+}
+
+int get_args_nr(const char *call)
+{
+	int nr = 0;
+	char *offset = (char *)call;
+
+	for (; get_next_arg(&offset); nr++);
+
+	return nr;
+}
+
 char *call_start_at_arg(const char *call, int arg_idx)
 {
-	char *ptr = (char *)call;
+	char *offset = (char *)call;
+	char *ptr = NULL;
 
-	if (arg_idx > 1) {
-		while (--arg_idx) {
-			ptr = strchr(ptr, ',');
-			if (!ptr)
-				break;
-			ptr += 2;
+	if (arg_idx > 0) {
+		while (arg_idx--) {
+			ptr = get_next_arg(&offset);
 		}
 	} else if (arg_idx < 0) {
 		while (arg_idx++) {
@@ -47,10 +91,7 @@ char *call_start_at_arg(const char *call, int arg_idx)
 			ptr += 2;
 		}
 	} else if (arg_idx == 1) {
-		if (*ptr == '(')
-			ptr++;
-		if (*ptr == ')')
-			ptr = NULL;
+		ptr = get_next_arg(&offset);
 	}
 
 	return ptr;
