eb5d00ae2120bbfff4af19653c1ad19b1626ed32
Djalal Harouni <tixxdz@opendz.org>
2013-05-24 18:28:55 +0100

    Ahaggar: update the api-warnings with more calls
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

--- fncalls_checks/api-warnings.h
+++ fncalls_checks/api-warnings.h
@@ -70,3 +70,102 @@
 	//world readable
 	//world writable
 },
+{
+	.name = "strcpy",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.args = "^\\(.*, <addr_expr> <string_cst>\\s.*\\)$",
+		.msg_nomatch = "function 'strcpy' src argument is not a const"
+		},
+		{ NULL },
+	},
+},
+{
+	.name = "strcat",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.args = "^\\(.*, <addr_expr> <string_cst>\\s.*\\)$",
+		.msg_nomatch = "function 'strcat' src argument is not a const"
+		},
+		{ NULL },
+	},
+},
+{
+	.name = "bcopy",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.force_match = 1,
+		.msg = "the 'bcopy' function is obsolete. POSIX.1-2008 removed it specification"
+		},
+		{ NULL },
+	},
+},
+{
+	.name = "bzero",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.force_match = 1,
+		.msg = "the 'bzero' function is obsolete. POSIX.1-2008 removed it specification"
+		},
+		{ NULL },
+	},
+},
+{
+	.name = "atoi",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.force_match = 1,
+		.msg = "the 'atoi' function does not detect errors"
+		},
+		{ NULL },
+	}
+},
+{
+	.name = "atol",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.force_match = 1,
+		.msg = "the 'atol' function does not detect errors"
+		},
+		{ NULL },
+	}
+},
+{
+	.name = "getpw",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.force_match = 1,
+		.msg = "the 'getpw' function is dangerous and should not be used."
+		},
+		{ NULL },
+	}
+},
+{
+	.name = "ulimit",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.force_match = 1,
+		.msg = "the 'ulimit' function is marked obsolete by POSIX.1-2008"
+		},
+		{ NULL },
+	},
+},
+{
+	.name = "usleep",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.force_match = 1,
+		.msg = "the 'usleep' function is obsolete. POSIX.1-2008 removed it specification"
+		},
+		{ NULL },
+	},
+},
