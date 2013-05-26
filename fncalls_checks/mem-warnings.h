12a3c5e9c31e467b4edd775722fb41f4b92e4587
Djalal Harouni <tixxdz@opendz.org>
2013-05-26 15:31:09 +0100

    Ahaggar: add the mem-warnings file for different mem checks
    Includes const size checks for:
    * memset(), memcpy(), strncpy(), strncat(), memmove() and memcmp()
    
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

---
+++ fncalls_checks/mem-warnings.h
@@ -0,0 +1,87 @@
+/* Add checks for __builitin_* functions */
+{
+	.name = "memset",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.match_args_func = memset_chk_size,
+		},
+		{ NULL },
+	}
+},
+{
+	.name = "memcpy",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.match_args_func = memcpy_chk_size,
+		},
+		{ NULL },
+	}
+},
+{
+	.name = "strncpy",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.match_args_func = strncpy_chk_size,
+		},
+		{ NULL },
+	}
+},
+{
+	.name = "strncat",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.match_args_func = strncat_chk_size,
+		},
+		{ NULL },
+	}
+},
+{
+	.name = "memmove",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.match_args_func = memmove_chk_size,
+		},
+		{ NULL },
+	}
+},
+{
+	.name = "memcmp",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.match_args_func = memcmp_chk_size,
+		},
+		{ NULL },
+	}
+},
+/* bcopy() was removed by POSIX.1-2008
+ * Add it the an api-warnings.h file
+{
+	.name = "bcopy",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.match_args_func = bcopy_chk_size,
+		},
+		{ NULL },
+	}
+},
+*/
+/* bzero() was removed by POSIX.1-2008
+ * Add it the an api-warnings.h file
+{
+	.name = "bzero",
+	.patterns = (struct pattern_match []) {
+		{
+		.active = 1,
+		.match_args_func = bzero_chk_size,
+		},
+		{ NULL },
+	}
+},
+*/
