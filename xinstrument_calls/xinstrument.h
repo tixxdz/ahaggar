84140a7dc4dbb2145235ced2cd00d1d0f9511813
Djalal Harouni <tixxdz@opendz.org>
2013-06-23 16:11:38 +0100

    Ahaggar: add the xinstrument.h header file
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

---
+++ xinstrument_calls/xinstrument.h
@@ -0,0 +1,29 @@
+/*
+ * The 'Ahaggar framework'
+ *
+ * Copyright (C) 2012-2013  Djalal Harouni <tixxdz@opendz.org>
+ * Copyright (C) 2012-2013  LIRE Laboratory.
+ * University Constantine 2, Algeria.
+ *
+ *   Author(s): Djalal Harouni <tixxdz@opendz.org>
+ *              Pr. Mohamed Benmohammed
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _XINSTRUMENT_H
+#define _XINSTRUMENT_H
+
+#include "xinstrument-utils.h"
+#include "compiletime-size.h"
+
+#endif /* end of _XINSTRUMENT_H */
