da4a1132428d3e66188d22fd6c92d3f33f2574f8
Djalal Harouni <tixxdz@opendz.org>
2013-07-19 16:59:57 +0100

    Ahaggar: add comments to the __init__.py module
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

--- scripts/pahaggar/__init__.py
+++ scripts/pahaggar/__init__.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python
-__version__ = '20130110'
+# pAhaggar Python module
+#
+# Copyright (C) 2012-2013  Djalal Harouni <tixxdz@opendz.org>
+# Copyright (C) 2012-2013  LIRE Laboratory.
+# University Constantine 2, Algeria.
+#
+#	Author(s): Djalal Harouni <tixxdz@opendz.org>
+#		   Pr. Mohamed Benmohammed
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License.
+#
 
-if __name__ == '__main__': print __version__
+__version__ = '20130110'
+version = __version__
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
