967b01da3608b84f471e61216a0060106ef2092c
Djalal Harouni <tixxdz@opendz.org>
2013-12-14 11:53:53 +0100

    Ahaggar: add kernel memory functions report
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

---
+++ fncalls_checks/kmemory-reports.h
@@ -0,0 +1,21 @@
+{
+	.name = "copy_from_user",
+},
+{
+	.name = "strncpy",
+},
+{
+	.name = "memset",
+},
+{
+	.name = "memcpy",
+},
+{
+	.name = "strncat",
+},
+{
+	.name = "__compiletime_size_overflow",
+},
+{
+	.name = "__compiletime_ptr_overflow",
+},
