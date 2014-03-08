983c6352edb466557acf3370b92bb188f3ef2261
Djalal Harouni <tixxdz@opendz.org>
2014-03-08 01:20:30 +0100

    ahaggar-ast: update pointer_type integer_cst to json
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

--- ahaggar-ast.c
+++ ahaggar-ast.c
@@ -468,12 +468,21 @@ static tree ahg_ast_tree_walker(tree *b, int *walk_subtrees,
 	case INTEGER_CST:
 		output_node_addr(buffer, node, ast->flags);
 
-		if (TREE_CODE(TREE_TYPE(node)) == POINTER_TYPE)
-			output_expr_code(buffer,
-					 TREE_TYPE(node),
-					 ast->flags);
+		if (TREE_CODE(TREE_TYPE(node)) == POINTER_TYPE) {
+			output_node_init(TREE_TYPE(node), ast);
+			output_char(buffer, '{');
+			walker_depth++;
+			output_indent_to_newline(buffer,
+						 walker_depth * INDENT);
+		}
 		output_expr_code(buffer, node, ast->flags);
 		output_int_cst(buffer, node);
+		if (TREE_CODE(TREE_TYPE(node)) == POINTER_TYPE) {
+			walker_depth--;
+			output_indent_to_newline(buffer,
+						 walker_depth * INDENT);
+			output_char(buffer, '}');
+		}
 		*walk_subtrees = 0;
 		break;
 
